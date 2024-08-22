d1cb00d1d8b82706ffba6e900fdbcdff2f93038e
Cristobal Varas <cristobal.varasp@gmail.com>
2024-08-21 22:19:33 -0400

    agregando modal y logica js

--- assets/js/script_Formu.js
+++ assets/js/script_Formu.js
@@ -1,16 +1,16 @@
-document.getElementById("dataForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+// Obtener los valores del formulario
+const name = document.getElementById("inputName").value;
+const email = document.getElementById("inputEmail").value;
+const nickname = document.getElementById("inputNickname").value;
 
-  // Obtener los valores del formulario
-  const name = document.getElementById("inputName").value;
-  const email = document.getElementById("inputEmail").value;
-  const comentario = document.getElementById("inputComentario").value;
+// Insertar los valores en la tarjeta
 
-  // Insertar los valores en la tarjeta
-  document.getElementById("cardName").textContent = name;
-  document.getElementById("cardEmail").textContent = email;
-  document.getElementById("cardComentario").textContent = comentario;
-
-  // Mostrar la tarjeta
-  document.getElementById("cardContainer").classList.remove("d-none");
+// Mostrar la tarjeta
+document.getElementById("btnSubmit").addEventListener("click", function () {
+  console.log(name, email, nickname);
 });
+
+// Agregar datos a la tabla
+let tabla = document.getElementById("myTable");
+
+// funcionalidad para limpiar el form al enviar
