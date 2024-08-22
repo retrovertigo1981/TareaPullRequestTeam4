27095578be8d930da14c51677da03f7a5cb53886
Claudio <claudiomelin.martinez@gmail.com>
2024-08-21 21:19:38 -0400

    Se agrega carrusel, add H1,H2 mas DIV

--- assets/js/script_Formu.js
+++ assets/js/script_Formu.js
@@ -1,17 +1,16 @@
-document.getElementById('dataForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    // Obtener los valores del formulario
-    const name = document.getElementById('inputName').value;
-    const email = document.getElementById('inputEmail').value;
-    const comentario = document.getElementById('inputComentario').value;
-  
-    // Insertar los valores en la tarjeta
-    document.getElementById('cardName').textContent = name;
-    document.getElementById('cardEmail').textContent = email;
-    document.getElementById('cardComentario').textContent = comentario;
-  
-    // Mostrar la tarjeta
-    document.getElementById('cardContainer').classList.remove('d-none');
-  });
-  
+document.getElementById("dataForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Obtener los valores del formulario
+  const name = document.getElementById("inputName").value;
+  const email = document.getElementById("inputEmail").value;
+  const comentario = document.getElementById("inputComentario").value;
+
+  // Insertar los valores en la tarjeta
+  document.getElementById("cardName").textContent = name;
+  document.getElementById("cardEmail").textContent = email;
+  document.getElementById("cardComentario").textContent = comentario;
+
+  // Mostrar la tarjeta
+  document.getElementById("cardContainer").classList.remove("d-none");
+});
